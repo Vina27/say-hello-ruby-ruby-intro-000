20d235e76e9a73837e14faff55c136020cacac4a
Maysoon Tabali <mtab03@me.com>
2020-03-18 20:04:46 +0000

    Automatically backed up by Learn

--- say_hello.rb
+++ say_hello.rb
@@ -2,7 +2,7 @@
 #def say_hello(name = "Ruby programmer")
   #puts "Hello #{Kent Beck!l}"
 #end
-def say_hello(name="Ruby programmer")
+def say_hello(name="Ruby Programmer")
   puts "Hello " + name + "!"
 end
 say_hello("Kent Beck")
